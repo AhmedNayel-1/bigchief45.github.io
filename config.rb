90f3ea49a90d3277c45d2b1fe55d80648ebed6d5
Andres Alvarez <andresalvarez353@gmail.com>
2018-01-16 16:23:35 +0000

    Use Github avatar as avatar

--- config.rb
+++ config.rb
@@ -9,30 +9,14 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
 
 # General configuration
+set :avatar_url, 'https://avatars3.githubusercontent.com/u/6859247?s=460&v=4'
 
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
 end
-
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
 
 # Build-specific configuration
 configure :build do
